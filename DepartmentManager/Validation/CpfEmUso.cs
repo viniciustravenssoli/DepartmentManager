714c6f37a911ac5a36aace308f262e8141d39d67
Julia Ferreira <juliaferreiradecastro71@gmail.com>
2023-09-29 09:47:40 -0300

    Validação Cpf

---
+++ DepartmentManager/Validation/CpfEmUso.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using DepartmentManager.Data;
+using DepartmentManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DepartmentManager;
+
+public class CpfEmUso : ValidationAttribute
+    {
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            string cpf = (string)value;
+
+            AppDbContext context =
+                (AppDbContext)validationContext.GetService(typeof(AppDbContext));
+
+            Employee employee = context.Employees.FirstOrDefault
+                (f => f.CPF.Equals(cpf));
+                
+            if (employee == null)
+            {
+                //Caso de sucesso
+                return ValidationResult.Success;
+            }
+            //Caso de erro
+            return new ValidationResult("O CPF do funcionário já está em uso!");
+        }
+    }
+
+
